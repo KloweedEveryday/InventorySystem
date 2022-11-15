4fa2047096d798ef532c34a5d1226e653729ff8d
KloweedEveryday <113875351+KloweedEveryday@users.noreply.github.com>
2022-11-15 17:43:26 +0800

    Personel(Design)

---
+++ InventoryManagement/Models/Proccess.cs
@@ -0,0 +1,13 @@
+﻿using Beverages_Softdrinks_InventorySystem.Models;
+
+namespace InventoryManagement.Models
+{
+    public class Proccess
+    {
+        private static List<string> _proccessList = new List<string>();
+
+        public List<string> ProccessList { get => _proccessList; set => _proccessList = value; }
+        public IList<Personel_Details> Personel_Details { get; set; } = default!;
+    }
+
+}
